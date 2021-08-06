4c4270376e6d3e001336977e3df1f38685b3a9c8
rharshith2410 <68144219+rharshith2410@users.noreply.github.com>
2021-08-06 22:17:02 +0530

    Create Sizeof.cpp

---
+++ Sizeof.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of int: "<<sizeof(int)<<" bytes";
+    cout<<"\n size of float: "<<sizeof(float)<<" bytes";
+    cout<<"\n size of double: "<<sizeof(double)<<" bytes";
+    cout<<"\n size of char: "<<sizeof(char)<<" byte";
+}
